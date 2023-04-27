22f9b9e2e3454b6cfeaf104a31cd070270aa667e
Sankarkumarks <99599930+Sankarkumarks@users.noreply.github.com>
2023-04-27 12:46:18 +0530

    Update w1.sh

--- w1.sh
+++ w1.sh
@@ -1,8 +1,6 @@
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP
-./ngrok tcp --region in 3388 &>/dev/null &
 nohup ./ngrok tcp 3388 &>/dev/null &
-./ngrok tcp 3388 &>/dev/null &
 b='\033[1m'
 r='\E[31m'
 g='\E[32m'
@@ -27,7 +25,7 @@ echo "Wait"
 echo "===================================="
 echo "===================================="
 echo "Starting Windows"
-sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8192M -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic /dev/null 2>&1
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
