9aa669e926656f530eba1af4ef7d0ddd245ff3f0
Sankarkumarks <99599930+Sankarkumarks@users.noreply.github.com>
2023-04-27 12:32:22 +0530

    Update win10lite.sh

--- win10lite.sh
+++ win10lite.sh
@@ -1,38 +1,36 @@
+echo "===================================="
+echo "Download windows files"
+echo "===================================="
+curl -L -o w8x64.img https://bit.ly/akuhnetw8x64
+echo "===================================="
+echo "Download ngrok"
+echo "===================================="
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null &
-echo Please wait for installing...
-sudo apt update -y > /dev/null 2>&1
-echo "Installing QEMU (2-3m)..."
-sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
-echo Downloading Windows Disk...
-echo "Windows 10 x64 Lite On Gitpod, GCloud and GColab"
-echo Your VNC IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "Note: Use Right-Click Or Ctrl+C To Copy"
-echo "Please Keep this Tab to Open, Maximum Time 50h"
-echo "Support YT Channel-> Aank is ME"
-echo "LinK: https://aank.me/Youtube"
-b='\033[1m'
-r='\E[31m'
-g='\E[32m'
-c='\E[36m'
-endc='\E[0m'
-enda='\033[0m'
-# Branding
-
-printf """$c$b
- 
-██╗     ██╗███╗   ██╗ ██████╗  ██████╗  █████╗ ██╗  ██╗ ██████╗ ███████╗████████╗██╗███╗   ██╗ ██████╗ 
-██║     ██║████╗  ██║██╔════╝ ██╔════╝ ██╔══██╗██║  ██║██╔═══██╗██╔════╝╚══██╔══╝██║████╗  ██║██╔════╝ 
-██║     ██║██╔██╗ ██║██║  ███╗██║  ███╗███████║███████║██║   ██║███████╗   ██║   ██║██╔██╗ ██║██║  ███╗
-██║     ██║██║╚██╗██║██║   ██║██║   ██║██╔══██║██╔══██║██║   ██║╚════██║   ██║   ██║██║╚██╗██║██║   ██║
-███████╗██║██║ ╚████║╚██████╔╝╚██████╔╝██║  ██║██║  ██║╚██████╔╝███████║   ██║   ██║██║ ╚████║╚██████╔╝
-╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝
-    $r  Support YT Channel-> Aank is ME © 2022 $c https://aank.me/Youtube 
-          
-$endc$enda""";
+unzip ngrok-stable-linux-amd64.zip &>/dev/null &
+unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP
+./ngrok tcp --region in 3388 &>/dev/null &
+nohup ./ngrok tcp 3388 &>/dev/null &
+./ngrok tcp 3388 &>/dev/null &
+echo "===================================="
+echo Downloading File From akuh.net
+echo "===================================="
+apt-get install qemu /dev/null 2>&1
+echo "===================================="
+echo "Wait"
+echo "===================================="
+echo "===================================="
+echo "Starting Windows"
 sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic /dev/null 2>&1
-sleep 43210
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Ctrl+C To Copy"
+echo "Wait 1-2 minute to finish bot"
+echo "Dont Close This Tab"
+echo "Please support akuh.net thank you"
+echo "===================================="
+echo "===================================="
+sleep 43200
